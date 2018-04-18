802f75c960436e365900b5339a5952cad57ed93b
luis <lfflorian4@gmail.com>
2018-04-17 21:16:11 -0600

    Agregar archivos de proyecto.

---
+++ wsRenap/IRenap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wsRenap
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IRenap" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IRenap
+    {
+        [OperationContract]
+        void TieneArraigo(string dpi);
+
+        [OperationContract]
+        void EsMayorDeEdad(string dpi);
+
+        [OperationContract]
+        void EstadoPersona(string dpi);
+
+        [OperationContract]
+        void VigenciaDeDPI(string dpi);
+    }
+}
---
+++ wsRenap/Renap.svc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wsRenap
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Renap" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Renap.svc o Renap.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Renap : IRenap
+    {
+        public Renap()
+        {
+        }
+        
+        public void EsMayorDeEdad(string dpi)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void EstadoPersona(string dpi)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void TieneArraigo(string dpi)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void VigenciaDeDPI(string dpi)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
